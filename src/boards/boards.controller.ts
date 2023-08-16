2be45c4120428d5a8e517f908dddd42d220d66b6
DongHyeok Lim <jh07050@gmail.com>
2023-08-17 02:06:03 +0900

    Feat: updateBoardStatus

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
 import { createBoardDto } from './dto/create-board.dto';
 
 @Controller('/boards')
@@ -25,5 +25,10 @@ export class BoardsController {
   @Delete('/:id')
   deleteBoard(@Param('id') id: string): void {
     return this.boardsService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus) {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
