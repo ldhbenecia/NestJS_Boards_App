371bc80a0103a67a273643c1bdec1f42bd331a21
DongHyeok Lim <jh07050@gmail.com>
2023-08-17 01:49:38 +0900

    Feat: create-board.dto

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
+import { createBoardDto } from './dto/create-board.dto';
 
 @Controller('/boards')
 export class BoardsController {
@@ -12,7 +13,7 @@ export class BoardsController {
   }
 
   @Post()
-  createBoard(@Body('title') title: string, @Body('description') description: string): Board {
-    return this.boardsService.createBoards(title, description);
+  createBoard(@Body() createBoardDto: createBoardDto): Board {
+    return this.boardsService.createBoards(createBoardDto);
   }
 }
