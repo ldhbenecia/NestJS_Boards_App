9da85fad4c66dd770878e0de127209aa3ebdba46
DongHyeok Lim <jh07050@gmail.com>
2023-08-22 16:18:37 +0900

    Feat: 로그인 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './repositories/user.repository';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -9,7 +11,18 @@ export class AuthService {
     private userRepository: UserRepository,
   ) {}
 
-  async signUp(authCredentialsDto): Promise<void> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUsers(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ where: { username: String(username) } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'Login Success';
+    } else {
+      throw new UnauthorizedException('Login Failed');
+    }
   }
 }
